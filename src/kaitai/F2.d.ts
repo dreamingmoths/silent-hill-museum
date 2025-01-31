12603aad46b0ae46a9af7d7200056a378e6fd3d7
laura ann <lauraann@laura-ann-gaming.local>
2025-01-30 23:40:32 -0800

    impl fuzz test for f2

---
+++ src/kaitai/F2.d.ts
@@ -0,0 +1,37 @@
+// This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+declare class F2 {
+  constructor(io: any, parent?: any, root?: any);
+  __type: 'F2';
+  _io: any;
+
+  absValue: number;
+  exponent: number;
+  floatValue: number;
+  fraction: number;
+  infinity: number;
+  isSpecial: boolean;
+  isSubnormal: boolean;
+  nan: number;
+  normalValue: number;
+  offsetExp: number;
+  powExp: F2.Pow2;
+  sign: number;
+  specialValue: number;
+  subnormalValue: number;
+  bytes: number;
+}
+
+declare namespace F2 {
+  class Pow2 {
+    constructor(io: any, parent?: any, root?: any);
+    __type: 'Pow2';
+    _io: any;
+
+    val: number;
+    exponent: number;
+  }
+}
+
+export = F2;
+export as namespace F2;
---
+++ src/tests/f2.spec.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "vitest";
+import F2 from "../kaitai/F2";
+
+const mockBinaryFile = (n: number) => ({
+  readU2le() {
+    return n;
+  },
+});
+
+const constructHalfFloat = (n: number) => new F2(mockBinaryFile(n)).floatValue;
+
+/**
+ * @see {@link https://stackoverflow.com/a/8796597}
+ */
+const decodeFloat16 = (binary: number) => {
+  var exponent = (binary & 0x7c00) >> 10,
+    fraction = binary & 0x03ff;
+  return (
+    (binary >> 15 ? -1 : 1) *
+    (exponent
+      ? exponent === 0x1f
+        ? fraction
+          ? NaN
+          : Infinity
+        : Math.pow(2, exponent - 15) * (1 + fraction / 0x400)
+      : 6.103515625e-5 * (fraction / 0x400))
+  );
+};
+
+test("fuzz conversion", () => {
+  const RANGE = 0x7bff;
+  for (let i = 0; i < 8888; i++) {
+    const n = Math.random() * RANGE;
+    const expected = decodeFloat16(n);
+    const actual = constructHalfFloat(n);
+    if (!Number.isFinite(expected)) {
+      // this implementation doesn't handle infinity/NaN properly
+      expect(actual).toEqual(0);
+      continue;
+    }
+    expect(Math.abs(expected)).toBeCloseTo(actual);
+  }
+});
