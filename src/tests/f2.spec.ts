47e14a84f14d87bcad7e9412a82a86d658c110d4
laura ann <lauraann@laura-ann-gaming.local>
2025-01-31 21:26:56 -0800

    refactor tests

--- src/tests/f2.spec.ts
+++ src/tests/f2.spec.ts
@@ -1,64 +1,83 @@
 import { expect, test } from "vitest";
-import F2 from "../kaitai/F2";
+import KaitaiFloat16 from "../kaitai/F2";
 
+type BinaryFile = ReturnType<typeof mockBinaryFile>;
 const mockBinaryFile = (n: number) => ({
   readU2le() {
     return n;
   },
 });
 
-const constructHalfFloat = (n: number) => new F2(mockBinaryFile(n)).floatValue;
+class Float16 {
+  public static fromBinaryFile(file: BinaryFile) {
+    return new KaitaiFloat16(file).floatValue;
+  }
 
-/**
- * @see {@link https://stackoverflow.com/a/8796597}
- */
-const decodeFloat16 = (binary: number) => {
-  var exponent = (binary & 0x7c00) >> 10,
-    fraction = binary & 0x03ff;
-  return (
-    (binary >> 15 ? -1 : 1) *
-    (exponent
-      ? exponent === 0x1f
-        ? fraction
-          ? NaN
-          : Infinity
-        : Math.pow(2, exponent - 15) * (1 + fraction / 0x400)
-      : 6.103515625e-5 * (fraction / 0x400))
-  );
+  /**
+   * @see {@link https://stackoverflow.com/a/8796597}
+   */
+  public static fromBytes(bytes: number) {
+    var exponent = (bytes & 0x7c00) >> 10,
+      fraction = bytes & 0x03ff;
+    return (
+      (bytes >> 15 ? -1 : 1) *
+      (exponent
+        ? exponent === 0x1f
+          ? fraction
+            ? NaN
+            : Infinity
+          : Math.pow(2, exponent - 15) * (1 + fraction / 0x400)
+        : 6.103515625e-5 * (fraction / 0x400))
+    );
+  }
+}
+
+const getRandomInputs = (range: number, size: number) => {
+  const numbers: number[] = [];
+  const mockFiles: BinaryFile[] = [];
+  for (let i = 0; i < size; i++) {
+    const n = Math.round(Math.random() * range);
+    numbers[i] = n;
+    mockFiles[i] = mockBinaryFile(n);
+  }
+  return { numbers, mockFiles };
 };
 
 test("fuzz conversion", () => {
   const RANGE = 0x7bff;
   const TRIALS = 88888;
+  const { numbers, mockFiles } = getRandomInputs(RANGE, TRIALS);
 
   for (let i = 0; i < TRIALS; i++) {
-    const n = Math.round(Math.random() * RANGE);
-    const expected = decodeFloat16(n);
-    const actual = constructHalfFloat(n);
+    const n = numbers[i];
+    const f = mockFiles[i];
+    const expected = Float16.fromBytes(n);
+    const actual = Float16.fromBinaryFile(f);
     if (!Number.isFinite(expected)) {
       // this implementation doesn't handle infinity/NaN properly
       expect(actual).toEqual(0);
       continue;
     }
-    expect(Math.abs(expected)).toBeCloseTo(actual);
+    expect(Math.abs(expected), `for ${n}`).toBeCloseTo(actual);
   }
 });
 
 test("benchmark", () => {
   const RANGE = 0x7bff;
   const TRIALS = 88888;
+  const { numbers, mockFiles } = getRandomInputs(RANGE, TRIALS);
 
   console.time("native js");
   for (let i = 0; i < TRIALS; i++) {
-    const n = Math.round(Math.random() * RANGE);
-    decodeFloat16(n);
+    const n = numbers[i];
+    Float16.fromBytes(n);
   }
   console.timeEnd("native js");
 
   console.time("kaitai");
   for (let i = 0; i < TRIALS; i++) {
-    const n = Math.round(Math.random() * RANGE);
-    constructHalfFloat(n);
+    const f = mockFiles[i];
+    Float16.fromBinaryFile(f);
   }
   console.timeEnd("kaitai");
 });
